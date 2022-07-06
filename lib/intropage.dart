425afa2a52590478456f58a3ba23c04119b83c03
lisadw10 <Kusen Abdullah>
2022-07-06 13:52:33 +0700

    remove unsed code

--- lib/intropage.dart
+++ lib/intropage.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:introduction_screen/introduction_screen.dart';
-import 'home.dart';
+
 import 'login.dart';
 
 class IntroPage extends StatefulWidget {
